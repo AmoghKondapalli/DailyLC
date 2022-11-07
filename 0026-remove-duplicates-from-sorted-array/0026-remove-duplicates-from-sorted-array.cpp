028827c1b0706abb23d923a413488444fdc69d59
Amogh_Kondapalli <ak3626@srmist.edu.in>
2022-11-07 18:30:06 +0530

    edit

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 1;i<nums.size();i++){
             if(nums[i]!=nums[i-1]){
                 nums[k] = nums[i];
-                k++;
+                ++k;
             }
         }
         return k;
